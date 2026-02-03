e1929bcf2ced3e4efce55960946e642ef62727a0
Wangscorp <edwinwang62@gmail.com>
2026-02-03 11:57:42 +0300

    Made Corrections

--- src/joke.js
+++ src/joke.js
@@ -1,18 +1,23 @@
-const request = require("request-promise");
-
-const options = {
-  method: "GET",
-  uri: "https://icanhazdadjoke.com/",
-  headers: {
-    Accept: "application/json",
-    "User-Agent": "Writing JavaScript action GitHub Skills exercise.",
-  },
-  json: true,
-};
-
 async function getJoke() {
-  const res = await request(options);
-  return res.joke;
+  try {
+    const response = await fetch("https://icanhazdadjoke.com/", {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "User-Agent": "Writing JavaScript action GitHub Skills exercise.",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data.joke;
+  } catch (error) {
+    console.error("Error fetching joke:", error);
+    return "Why don't scientists trust atoms? Because they make up everything!";
+  }
 }
 
 module.exports = getJoke;
